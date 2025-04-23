5f4262b344e0cbee1eb56d33edc5d819696afd4d
Mark Farrell <mark.andrew.farrell@gmail.com>
2025-04-23 10:45:38 +1000

    add client provider init command

--- main.go
+++ main.go
@@ -438,6 +438,25 @@ Exit code: 0 if all entries are valid, 1 if any warnings or errors are found.`,
 
 	providerCmd.AddCommand(providerListCmd)
 
+	providerInitCmd := &cobra.Command{
+		Use:     "init",
+		Short:   "Initialize client provider configuration",
+		Example: `  opkssh provider init`,
+		Args:    cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := config.CreateDefaultClientConfig(configPathArg, afero.NewOsFs())
+
+			if err != nil {
+				log.Fatal("Unable to initialize provider configuration. ", err)
+			}
+			return nil
+		},
+	}
+
+	providerInitCmd.Flags().StringVar(&configPathArg, "config-path", "", "Path to the client config file. Default: ~/.opk/config.yml on linux and %APPDATA%\\.opk\\config.yml on windows.")
+
+	providerCmd.AddCommand(providerInitCmd)
+
 	clientCmd.AddCommand(providerCmd)
 
 	rootCmd.AddCommand(clientCmd)
