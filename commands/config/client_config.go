523f6158c9f397c2530f69b0e99035ae3c05bd43
Aaron <akl@riact.eu>
2025-08-14 19:02:36 +0200

    extract config path resolution

--- commands/config/client_config.go
+++ commands/config/client_config.go
@@ -92,13 +92,8 @@ func GetClientConfigFromFile(configPath string, Fs afero.Fs) (*ClientConfig, err
 }
 
 func CreateDefaultClientConfig(configPath string, Fs afero.Fs) error {
-	if configPath == "" {
-		var err error
-		configPath, err = GetDefaultClientConfigPath()
-
-		if err != nil {
-			return err
-		}
+	if err := ResolveClientConfigPath(&configPath); err != nil {
+		return err
 	}
 
 	if _, err := Fs.Stat(configPath); err == nil {
